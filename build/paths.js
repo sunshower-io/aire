2278feee5922f37f710506c43adead7673f02894
josiahhaswell <josiah.haswell@gmail.com>
2018-01-20 19:17:05 -0700

    adding typescript

--- build/paths.js
+++ build/paths.js
@@ -2,13 +2,15 @@ var appRoot = 'src/';
 var outputRoot = 'dist/';
 
 module.exports = {
-  root: appRoot,
-  source: appRoot + '**/*.js',
-  html: appRoot + '**/*.html',
-  css: appRoot + '**/*.css',
-  style: 'styles/**/*.css',
-  output: outputRoot,
-  doc: './doc',
-  e2eSpecsSrc: 'test/e2e/src/*.js',
-  e2eSpecsDist: 'test/e2e/dist/'
+    root: appRoot,
+    source: appRoot + '**/*.ts',
+    html: appRoot + '**/*.html',
+    css: appRoot + '**/*.css',
+    style: 'styles/**/*.css',
+    output: outputRoot,
+    doc: './doc',
+    dtsSrc: [
+        './typings/**/*.d.ts',
+        './custom_typings/**/*.d.ts'
+    ]
 };
--- build/tasks/build.js
+++ build/tasks/build.js
@@ -1,7 +1,11 @@
 let gulp = require('gulp');
 let runSequence = require('run-sequence');
-let to5 = require('gulp-babel');
 let paths = require('../paths');
+var typescript = require('gulp-typescript');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
+var changed = require('gulp-changed');
+var sourcemaps = require('gulp-sourcemaps');
 let compilerOptions = require('../babel-options');
 let assign = Object.assign || require('object.assign');
 
@@ -21,34 +25,30 @@ gulp.task('build-css', function() {
     .pipe(gulp.dest(paths.output + 'system'));
 });
 
-gulp.task('build-es2015', function() {
-  return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions.es2015())))
-    .pipe(gulp.dest(paths.output + 'es2015'));
-});
 
-gulp.task('build-commonjs', function() {
-  return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions.commonjs())))
-    .pipe(gulp.dest(paths.output + 'commonjs'));
-});
-
-gulp.task('build-amd', function() {
-  return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions.amd())))
-    .pipe(gulp.dest(paths.output + 'amd'));
-});
-
+var typescriptCompiler = typescriptCompiler || null;
 gulp.task('build-system', function() {
-  return gulp.src(paths.source)
-    .pipe(to5(assign({}, compilerOptions.system())))
-    .pipe(gulp.dest(paths.output + 'system'));
+    if(!typescriptCompiler) {
+        typescriptCompiler = typescript.createProject('tsconfig.json', {
+            "typescript": require('typescript')
+        });
+    }
+
+    return gulp.src(paths.dtsSrc.concat(paths.source))
+        .pipe(plumber({errorHandler: notify.onError('Error: <%= error.message %>')}))
+        .pipe(changed(paths.output, {extension: '.ts'}))
+        .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(typescriptCompiler())
+        .pipe(sourcemaps.write('.', {includeContent: false, sourceRoot: '/src'}))
+        .pipe(gulp.dest(paths.output));
 });
+
+
 
 gulp.task('build', function(callback) {
   return runSequence(
     'clean',
-    ['build-html', 'build-css', 'build-es2015', 'build-commonjs', 'build-amd', 'build-system'],
+    ['build-html', 'build-css', 'build-system'],
     callback
   );
 });
