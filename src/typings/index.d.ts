382ae37a930e8f05137c73d8d4977c8d86bf5b1f
josiahhaswell <josiah.haswell@gmail.com>
2018-01-23 16:42:44 -0700

    Adding UUID definition

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,7 +1,17 @@
-declare module "sunshower/lang/uuid" {
-    
+declare module "sunshower/lang" {
     export class UUID {
+       
+        public readonly value : string;
         
+        constructor(value: string);
+
+        static random(): string;
+        
+        static fromString(str:string) : UUID;
+        
+        static randomBytes() : number[];
+        
+        static randomUUID() : UUID;
     }
-    
+
 }
