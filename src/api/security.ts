17f22774c842e0e1583b576af84a568818d4ed7c
josiahhaswell <josiah.haswell@gmail.com>
2018-01-23 19:45:45 -0700

    Updating tsd

---
+++ src/api/security.ts
@@ -0,0 +1,2 @@
+export * from 'aire/api/security/model'
+export * from 'aire/api/security/service'
--- src/api/security/service.ts
+++ src/api/security/service.ts
@@ -1,6 +1,8 @@
-import {HttpClient} from "aurelia-fetch-client";
 import {BooleanValue} from "aire/lib/types";
+import {autoinject} from "aurelia-framework";
+import {HttpClient} from "aurelia-fetch-client";
 
+@autoinject
 export class SecurityService {
     
     constructor(private client:HttpClient) {
