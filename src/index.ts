b979872a9b4302e69423449b182668d136090e03
josiahhaswell <josiah.haswell@gmail.com>
2018-01-27 12:32:32 -0700

    Adding authentication manager

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
 import {Events} from "aire/components/events";
 import {EventAggregator} from "aurelia-event-aggregator";
+import {
+    SecurityService, 
+    AuthenticationManager
+} from "aire/api/security";
 
 export function configure(config: FrameworkConfiguration) {
     let aurelia = config.aurelia,
         container = aurelia.container;
     
     config.postTask(t => {
+        let service = container.get(SecurityService),
+            authManager = new AuthenticationManager(service);
+        container.registerInstance(AuthenticationManager, authManager);
         Events.setEventAggregator(container.get(EventAggregator));
     });
+    
+    
     
     config.globalResources([
         './layout', 
