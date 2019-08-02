5c85c2861a96e6c80922d76cfcaa2117d433bf00
Josiah Haswell <josiah.haswell@gmail.com>
2019-08-01 19:46:13 -0600

    Adding stuff

--- build/paths.js
+++ build/paths.js
@@ -18,7 +18,7 @@ module.exports = {
   html: appRoot + '**/*.html',
   css: appRoot + '**/*.css',
   style: 'styles/**/*.css',
-  output: [outputRoot, exporSrvtRoot],
+  output: outputRoot,
   exportSrv: exporSrvtRoot,
   // locales: 'locales/**/*.yml',
   locales: ['en'],
