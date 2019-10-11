c81009ab635dbdd467e052a80006901ab82f0109
lisagumerman <lisajgumerman@gmail.com>
2019-10-11 17:56:35 -0600

    Adding documentation for aire-table's current incarnation, and removing test pug from applications.pug

--- src/main/component/table.ts
+++ src/main/component/table.ts
@@ -9,6 +9,50 @@ import {
 import {dom} from "aire/core/dom";
 import {AireColumn} from "aire/component/column";
 
+/**
+ *
+ * <p class="uk-text-lead">This component allows for easy table generation</p>
+ *
+ * @description
+ * Custom element for laying out a table.
+ *
+ * @section {Usage}
+ *
+ * The `aire-table` is a table with an optional caption, header and footer. Row data is provided as an array of arrays.
+ *
+ * ..ex
+ * aire-table(source.bind="[['row 1 col 1', 'row 1 col 2'], ['row 2 col 1', 'row 2 col 2'], ['row 3 col 1', 'row 3 col 2']]" columns.bind="['col 1', 'col 2']" striped hover footer caption="A Sample Table")
+ *
+ * @section {Style and Size Modifiers}
+ *
+ * Add one or more of `divider`, `striped`, `hover`, `justify`, `middle` and `responsive` to aire-table to change its appearance and behavior. Add `small` or `large` to change the default row padding.
+ *
+ * ..attr
+ * {divider} Add this attribute to add a line between each row
+ *
+ * ..attr
+ * {striped} Add this attribute to give odd-numbered rows a different background color
+ *
+ * ..attr
+ * {hover} Add this attribute to give rows a different appearance when hovered over
+ *
+ * ..attr
+ * {justify} Add this attribute to remove the padding of the first and last column
+ *
+ * ..attr
+ * {middle} Add this to vertically align all cell contents to the middle
+ *
+ * ..attr
+ * {responsive} Add this attribute if you want the table to stack on small screens
+ *
+ * ..attr
+ * {small} Add this attribute to decrease the size of table rows
+ *
+ * ..attr
+ * {large} Add this attribute to increase the size of table rows
+ *
+ */
+
 @inject(Element)
 @containerless
 @customElement('aire-table')
